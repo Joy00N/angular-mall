34b862ae1e1062ff9df2ec75b4300f455172b88f
Joy <38506754+Joy00N@users.noreply.github.com>
2020-01-17 00:09:33 -0500

    Revert "Feature/a"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  projectTitle : string = 'Yoon World';
+  pageTitle : string = 'Yoon Factory';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductComponent } from './products/product-list.component';
-import { StarComponent } from './shared/star.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductComponent,
-    StarComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
+    BrowserModule
   ],
   bootstrap: [AppComponent]
 })
